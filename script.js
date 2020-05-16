cbd9500a49e4eae1cc1d0e616e5919220ef824a6
Strizhenok Matvey <strizhenok95@gmail.com>
2020-05-17 00:03:10 +0300

    checkbox, all \Done\ \Restore\ \Remove\

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("lolol");
-
 const selectAllButton = document.getElementById("test");
 const InputText = document.getElementById("input")
 const ulElement = document.getElementById("list")
@@ -15,16 +13,25 @@ selectAllButton.addEventListener('click', pressButtonFunc)
 InputText.addEventListener('keydown', event => {
     if(event.key==="Enter" || event.keyCode === 13)
     {
-        todoList.push(InputText.value)
-        InputText.value = "";      
+        todoList.unshift({
+            content: InputText.value,
+            done: false,
+            selected: false            
+        })
+
+        InputText.value = "";    
+
         upgradeTask()
     }
 })
 
 function upgradeTask(){
     ulElement.innerHTML = ''
-    for(item of todoList)
+    //for(const item of todoList)
+    for(let index = 0; index < todoList.length; index++)
     {
+        const item = todoList[index]
+
         const liElement = document.createElement('li')
         liElement.className = 'list-group-item'
         ulElement.append(liElement)
@@ -37,23 +44,74 @@ function upgradeTask(){
         divElement.append(checkboxElement)
         checkboxElement.type = 'checkbox'
         checkboxElement.className = 'form-check-input'
-
+        checkboxElement.id = 'item' + index
+        checkboxElement.checked = item.selected
+       
         const labelElement = document.createElement('label')
         divElement.append(labelElement)
         labelElement.className = 'form-check-label'
-        labelElement.setAttribute('for', 'examplecheck1')
-        labelElement.innerText = item
+        if(item.done){
+            labelElement.className += ' todoDone'
+            //console.log(item.content, labelElement.className)
+        }
+        labelElement.setAttribute('for', 'item' + index)
+        labelElement.innerText = item.content
 
-        const buttonDone = document.createElement('button')
-        divElement.append(buttonDone)
-        buttonDone.type = 'button'
-        buttonDone.className = 'btn btn-primary'
-        buttonDone.innerText = 'Done'
+        const buttonDoneElement = document.createElement('button')
+        divElement.append(buttonDoneElement)
+        buttonDoneElement.type = 'button'
+        buttonDoneElement.className = 'btn btn-primary'
+        buttonDoneElement.innerText = 'Done'
 
-        const buttonRemove = document.createElement('button')
-        divElement.append(buttonRemove)
-        buttonRemove.type = 'button'
-        buttonRemove.className = 'btn btn-danger'
-        buttonRemove.innerText = 'Remove'
+        const buttonRemoveElement = document.createElement('button')
+        divElement.append(buttonRemoveElement)
+        buttonRemoveElement.type = 'button'
+        buttonRemoveElement.className = 'btn btn-danger'
+        buttonRemoveElement.innerText = 'Remove'
+
+        buttonDoneElement.addEventListener('click', () => {
+            item.done = !item.done 
+            upgradeTask()
+        })
+
+        checkboxElement.addEventListener('change', () => {
+            item.selected = checkboxElement.checked
+        })
     }
 }
+
+document.getElementById('doneAction').addEventListener('click', () => {
+    for(const item of todoList){
+        if(item.selected){
+            item.done = true
+            item.selected = false
+        } 
+    }
+
+    upgradeTask()
+})
+
+document.getElementById('restoreAction').addEventListener('click', () => {
+    for(const item of todoList){
+        if(item.selected){
+            item.done = false
+            item.selected = false
+        } 
+    }
+
+    upgradeTask()
+})
+
+document.getElementById('removeAction').addEventListener('click', () => {
+    const quantityItems = todoList.length
+    
+    for(let index = quantityItems-1; index >= 0; index--){
+        const item = todoList[index]
+
+        if(item.selected){  
+            todoList.splice(index, 1)
+        } 
+    }
+
+    upgradeTask()
+})
