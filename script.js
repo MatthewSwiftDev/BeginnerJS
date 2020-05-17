7068f3e0094bc0da977cd020548e7963c163ad88
Strizhenok Matvey <strizhenok95@gmail.com>
2020-05-18 00:34:09 +0300

    Version 1.0

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-const selectAllButton = document.getElementById("test");
 const InputText = document.getElementById("input")
 const ulElement = document.getElementById("list")
+const actionPanel1Element = document.getElementById('actionPanel1')
+const actionPanel2Element = document.getElementById('actionPanel2')
 
 let todoList=[]
 
-// При нажатии на клавишу, 
-function pressButtonFunc() {
-    console.log("Pressed the button");
-}
-selectAllButton.addEventListener('click', pressButtonFunc)
+// Панель с действимя изначальна не видна
+actionPanel2Element.style.display = 'none'
 
+// Если строка не пустая и нажимаем Enter, то заносим строку в todoList
 InputText.addEventListener('keydown', event => {
-    if(event.key==="Enter" || event.keyCode === 13)
+    if((event.key==="Enter" || event.keyCode === 13) && InputText.value)
     {
         todoList.unshift({
             content: InputText.value,
@@ -25,9 +24,10 @@ InputText.addEventListener('keydown', event => {
     }
 })
 
+// Вывод всех задач
 function upgradeTask(){
     ulElement.innerHTML = ''
-    //for(const item of todoList)
+
     for(let index = 0; index < todoList.length; index++)
     {
         const item = todoList[index]
@@ -52,31 +52,50 @@ function upgradeTask(){
         labelElement.className = 'form-check-label'
         if(item.done){
             labelElement.className += ' todoDone'
-            //console.log(item.content, labelElement.className)
         }
         labelElement.setAttribute('for', 'item' + index)
         labelElement.innerText = item.content
 
-        const buttonDoneElement = document.createElement('button')
-        divElement.append(buttonDoneElement)
-        buttonDoneElement.type = 'button'
-        buttonDoneElement.className = 'btn btn-primary'
-        buttonDoneElement.innerText = 'Done'
+        // Если задача не выполнена, то отображается только кнопка "Done"
+        if(!item.done){
+            const buttonDoneElement = document.createElement('button')
+            divElement.append(buttonDoneElement)
+            buttonDoneElement.type = 'button'
+            buttonDoneElement.className = 'btn btn-primary'
+            buttonDoneElement.innerText = 'Done'
+            buttonDoneElement.style = 'float: right'
 
-        const buttonRemoveElement = document.createElement('button')
-        divElement.append(buttonRemoveElement)
-        buttonRemoveElement.type = 'button'
-        buttonRemoveElement.className = 'btn btn-danger'
-        buttonRemoveElement.innerText = 'Remove'
-
-        buttonDoneElement.addEventListener('click', () => {
-            item.done = !item.done 
-            upgradeTask()
-        })
-
+            buttonDoneElement.addEventListener('click', () => {
+                item.done = !item.done 
+                upgradeTask()
+            })
+        }
+        // Иначе отображается только кнопка "Remove"
+        else{
+            const buttonRemoveElement = document.createElement('button')
+            divElement.append(buttonRemoveElement)
+            buttonRemoveElement.type = 'button'
+            buttonRemoveElement.className = 'btn btn-danger'
+            buttonRemoveElement.innerText = 'Remove'
+            buttonRemoveElement.style = 'float: right'
+            
+            buttonRemoveElement.addEventListener('click', () => {
+                    todoList = todoList.filter(i => i != item)
+                    upgradeTask()
+                })
+        }
+        
         checkboxElement.addEventListener('change', () => {
             item.selected = checkboxElement.checked
+            upgradeTask()
         })
+    }
+
+    // Если выбран хотя бы один элемент, то панель с действиями появляется
+    if(todoList.some(i => i.selected)) {
+        actionPanel2Element.style.display = 'block' 
+    } else {
+        actionPanel2Element.style.display = 'none'   
     }
 }
 
@@ -103,15 +122,26 @@ document.getElementById('restoreAction').addEventListener('click', () => {
 })
 
 document.getElementById('removeAction').addEventListener('click', () => {
-    const quantityItems = todoList.length
     
-    for(let index = quantityItems-1; index >= 0; index--){
-        const item = todoList[index]
+    // const quantityItems = todoList.length
+    
+    // for(let index = quantityItems-1; index >= 0; index--){
+    //     const item = todoList[index]
 
-        if(item.selected){  
-            todoList.splice(index, 1)
-        } 
-    }
+    //     if(item.selected){  
+    //         todoList.splice(index, 1)
+    //     } 
+    // }
+
+    todoList = todoList.filter(i => !i.selected)
+
+    upgradeTask()
+})
+
+document.getElementById('test').addEventListener('click', () => {
+    todoList.forEach(item => {
+        item.selected = true;
+    });
 
     upgradeTask()
 })
